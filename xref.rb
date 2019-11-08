bbc3c484894eb91be0c9480d2a051db5ce7801f0
wendybalaja <33009822+wendybalaja@users.noreply.github.com>
2019-11-07 23:48:02 -0500

    Update xref.rb

--- xref.rb
+++ xref.rb
@@ -9,13 +9,13 @@ end
 
 llvmIO.close
 
+#matches the entire debug line in llvm-dwarfdump
 debug_matcher = /^0x0000000000([0-9a-z]{6})\s+([0-9]*)\s+([0-9]*)\s+([0-9]*)\s+([0-9]*)\s+([0-9]*)\s+([a-z]+.*)$.*/
-file_name_1_matcher = /^(file_names)+(\[)\s+([0-9].*)+(\]:)$/
-file_name_2_matcher = /^(\s*)+(name:\s)+(.*)$/
 
-#addr_to_sourceline = {}
-#addr_to_sourceline["123"]="null"
-#puts addr_to_sourceline["123"]
+#matches the file number to file name
+file_name_1_matcher = /^(file_names)+(\[)\s+([0-9].*)+(\]:)$/
+file_name_2_matcher = /^(\s*)+(name:\s)+\"([a-z0-9]*\.[a-z])\"$/
+
 
 file_number = 0;
 file_name = ""
@@ -28,7 +28,6 @@ llvmOut.each{ |l|
   #Store file_number with file_name
   if file_name_1_matcher.match(l) then
     file_number =  Regexp.last_match(3).to_i
-   #puts file_number
   end
 
   if file_name_2_matcher.match(l) then
@@ -44,10 +43,9 @@ llvmOut.each{ |l|
     
     file_name = file_number_to_name[source_file_number]
     assembly_addr_to_filename[assembly_addr] = file_name
-    file = File.open("main.c") 
-    #TODO:file_num,file_name pair is stored,but not working properly here 
-    
-    #file = File.open(file_number_to_name[source_file_number])
+   
+    get_name = assembly_addr_to_filename[assembly_addr]
+    file = File.open(get_name) 
     source_line = IO.readlines(file)[sourceline_num-1]
     assembly_addr_to_sourceline[assembly_addr] = source_line
   
@@ -57,9 +55,29 @@ llvmOut.each{ |l|
 }
 
 
-#puts file_number_to_name[1]
-#puts addr_to_sourceline["401106"]
+#HTML_TEXT FRAMEWORK AND STYLE SHEET
+html_text ="
+<!DOCTYPE html>
+<head>
+ <title>xref for binary file</title>
+ <link rel=\"stylesheet\" type= \"text/css\" href=\"https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/prettify.css\">
+ <style type=\"text/css\">
+   *{ font-family: monospace; }
+   .grey{
+    color: #faa19b
+  }
 
+   .red{
+    color: #fa1f0f
+  }
+
+ </style>
+</head>
+
+<body>
+ <h2>xref for binary file</h2>
+ <table> 
+"
 
 ###### PARSE OBJDUMP 
 objdumpOut, objdumpIO = IO.pipe
@@ -69,57 +87,38 @@ end
 
 objdumpIO.close
 
+# match objdump output that begins with an address 
 objdump_addr_matcher = /^(\s*)+([0-9]*):+\s([0-9a-z].*)$/
+previous_sourceline = ""
 
-#objdumpOut.each{ |l|
-#  if objdump_addr_matcher.match(l) then
-#    objdump_assembly_addr = Regexp.last_match(2) 
-    #puts objdump_assembly_addr
-    #puts assembly_addr_to_sourceline.key?("401106")
-    #
-#    puts l
-#    if assembly_addr_to_sourceline.key?(objdump_assembly_addr) then
-#      puts assembly_addr_to_filename[objdump_assembly_addr]
-#      puts "***************"
-#      print  assembly_addr_to_sourceline[objdump_assembly_addr]
-#      puts "***************"
-#    end
-#  end
-#}
-
-#htmlFile = File.new("index.html","w+")
-html_text ="
-<!DOCTYPE html>
-<head>
- <title>xref for binary file
- </title>
- <style type=\"text/css\">
-   *{ font-family: monospace; }
- </style>
-</head>
-
-<body>
- <h2>xref for binary file</h2>
- <table> 
-"
-
+# loop through each output in objdump
+# check if matches with any sourceline
+# write to the html_text 
 objdumpOut.each{ |l|
   if objdump_addr_matcher.match(l) then
     objdump_assembly_addr = Regexp.last_match(2)
-    #puts objdump_assembly_addr
-    #puts assembly_addr_to_sourceline.key?("401106")
-    #
+    objdump_assembly_code = Regexp.last_match(3)
     html_text += "  <tr>\n"
-    html_text += " <td>\n"
-    html_text += "        "+ CGI.escapeHTML(l).gsub(/ /, "&nbsp;")+"<br>\n"
+    html_text += "  <td>\n"
+    html_text += " <a name=\"#{objdump_assembly_addr}\" href=\"##{objdump_assembly_addr}\">\n"
+    html_text += "        " + CGI.escapeHTML(objdump_assembly_addr).gsub(/ /,"&nbsp;")
+    html_text += "  </a>\n"
+    html_text += "        "+ CGI.escapeHTML(objdump_assembly_code).gsub(/ /, "&nbsp;")+"<br>\n"
     html_text += "        </td>\n"
-    html_text += "        </tr>\n"
     if assembly_addr_to_sourceline.key?(objdump_assembly_addr) then
-      puts assembly_addr_to_filename[objdump_assembly_addr]
-      puts "***************"
+      html_text += "  <td>\n"
+      html_text += "               "+ CGI.escapeHTML(assembly_addr_to_filename[objdump_assembly_addr]).gsub(/  /, "&nbsp;")+ "<br>\n"
+      if(assembly_addr_to_sourceline[objdump_assembly_addr]==previous_sourceline) then
+        html_text += " <td class =\"grey\">\n"
+      else
+        previous_sourceline = assembly_addr_to_sourceline[objdump_assembly_addr] 
+        html_text += "  <td class =\"red\">\n"
+      end 
+      html_text += "               "+ CGI.escapeHTML(previous_sourceline).gsub(/  /,"&nbsp;")+"<br>\n"
+      
       print  assembly_addr_to_sourceline[objdump_assembly_addr]
-      puts "***************"
     end
+    html_text += "        </tr>\n"
   end
 }
 html_text += "
@@ -127,5 +126,5 @@ html_text += "
 </body>
 </html>"
 
-
+ 
  File.write("./index.html",html_text)
