798aab68ed54514f3c9592c5d7c1d025988f38fc
wendybalaja <33009822+wendybalaja@users.noreply.github.com>
2019-11-07 23:48:47 -0500

    Added test files

---
+++ file1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "file2.h"
+
+int main (void){
+ printf("hello");
+ foo();
+ return 0;
+}
---
+++ file2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "file2.h"
+
+void foo(void){
+	int a = 1;
+	for (int i=0; i<2; i++){
+		printf("file2");
+	}
+	return;
+}
